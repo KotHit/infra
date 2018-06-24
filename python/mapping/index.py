838fb4fbccba4cdb6c24f6351d7c9ae77032eed1
kothit <pasha.kudrinskiy@gmail.com>
2018-06-24 13:29:57 +0300

    Change structure

--- python/mapping/index.py
+++ python/mapping/index.py
@@ -4,29 +4,30 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
+def mysql_con():
+    try:
+        cnx = mysql.connector.MySQLConnection(user='developer', password='dev',
+                                     host='10.10.10.2',
+                                     port=3306,
+                                     database='university')
+        print("All works fine")
+        data=pandas.read_sql("SELECT * FROM TABLE_NAME", con=cnx)
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+      else:
+        print(err)
+    else:
+      cnx.close()
 
-try:
-    cnx = mysql.connector.MySQLConnection(user='developer', password='dev',
-                                 host='10.0.10.10',
-                                 port=4000,
-                                 database='test')
-    print("All works fine")
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
+    name=list(data["University"])
+    rate=list(data["Rate"])
+    lat=list(data["Lat"])
+    lon=list(data["Lon"])
+    return lat, lon, name, rate
 
-#Parse file with data
-data=pandas.read_csv("data/university.csv")
-name=list(data["University"])
-rate=list(data["Rate"])
-lat=list(data["Lat"])
-lon=list(data["Lon"])
 
 def color_mark(rate):
     if rate<0:
@@ -43,7 +44,7 @@ def main():
     nau_position=[50.439895, 30.430243]
     map=folium.Map(location=nau_position, zoom_start=5)
     fg_name=folium.FeatureGroup(name="University Name")
-
+    lat, lon, name, rate=mysql_con()
     #Show Markers on map
     for lt, ln, nm, rt in zip(lat, lon, name, rate):
         color_temp, icon_temp=color_mark(rt)
@@ -62,8 +63,8 @@ def main():
     map.add_child(fg_poligon)
     map.add_child(fg_layer_one)
     map.add_child(folium.LayerControl())
-
-    map.save("index.html")
+    path='./data/index.html'
+    map.save(path)
 
 if __name__ == '__main__':
     main()
